6c4e54f3294d24fd4c8be1f9bdf7463b749923b9
CryoSnake22 <65683733+CryoSnake22@users.noreply.github.com>
2025-05-16 15:54:02 -0400

    add registration interface

--- app/src/main/java/com/example/localloop/registration.java
+++ app/src/main/java/com/example/localloop/registration.java
@@ -15,7 +15,7 @@ public class registration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registration), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
