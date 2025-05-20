94bf9fd69021702816372cbddbb5f5959bda4fcd
ShuChenDev <shu.chen.xm.work@gmail.com>
2025-05-20 15:25:56 -0400

    fix firebase gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-//    id("com.android.application")
-//    id("com.google.gms.google-services")
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
