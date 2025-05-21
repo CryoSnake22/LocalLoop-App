df3120a3050f28f05444dd2d0b4361a3be2cd2b0
ShuChenDev <shu.chen.xm.work@gmail.com>
2025-05-21 13:31:15 -0400

    Updated readme with todo, commented todo in loginAct and RegisAct

--- app/src/main/java/com/example/localloop/ui/auth/LoginActivity.java
+++ app/src/main/java/com/example/localloop/ui/auth/LoginActivity.java
@@ -17,41 +17,46 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+
+//Firebase guide https://firebase.google.com/docs/auth/android/password-auth#java_1
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String TAG = "LoginActivity";
-
-    private EditText emailField, passwordField;
-    private Button loginButton, signupButton;
     private FirebaseAuth mAuth;
+
+    private static final String TAG = "LoginActivity";
+    private EditText emailField, passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mAuth = FirebaseAuth.getInstance();
-        setContentView(R.layout.activity_login);
 
+        setContentView(R.layout.activity_login);
         emailField = findViewById(R.id.inputEmail);
         passwordField = findViewById(R.id.inputPassword);
-        loginButton = findViewById(R.id.buttonLogin);
-        signupButton = findViewById(R.id.buttonSignup);
+        Button loginButton = findViewById(R.id.buttonLogin);
+        Button signupButton = findViewById(R.id.buttonSignup);
 
+        //What happen after click login
         loginButton.setOnClickListener(v -> {
             String email = emailField.getText().toString().trim();
             String password = passwordField.getText().toString().trim();
 
+            //Basic check
             if (email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, "Email and password required", Toast.LENGTH_SHORT).show();
                 return;
             }
 
+            //Hard code admin login, bypasses firebase auth below
             if (email.equals("admin") && password.equals("XPI76SZUqyCjVxgnUjm0")) {
-                setContentView(R.layout.activity_admin_home);
+                setContentView(R.layout.activity_admin_home); //hardcode, only directo to admin layout thats all
                 Toast.makeText(this, "Admin login successful (bypassed Firebase)", Toast.LENGTH_SHORT).show();
                 return;
             }
 
+            //Firebase auth, checks email for you, https://firebase.google.com/docs/auth/android/password-auth#java_1
             mAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
@@ -68,15 +73,16 @@ public class LoginActivity extends AppCompatActivity {
                             }
                         }
                     });
-
         });
 
+        // Goes to RegistrationActivity, need to deferenciate participant and organizer
         signupButton.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
             startActivity(intent);
         });
     }
 
+    //Firebase auth thing https://firebase.google.com/docs/auth/android/password-auth#java_1
     @Override
     public void onStart() {
         super.onStart();
@@ -86,18 +92,22 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    //Can be filled, not nessasary
     private void reload() {
     }
 
+    //Called after successfully authed
     private void updateUI(FirebaseUser user) {
         if (user != null) {
             String email = user.getEmail();
 
+            //Basic check
             if (email == null) {
                 Toast.makeText(this, "User email not available", Toast.LENGTH_SHORT).show();
                 return;
             }
 
+            //Hard coded
             if (1 == 1) {
                 setContentView(R.layout.activity_organizer_home);
             } else if (email.contains("admin")) {
--- app/src/main/java/com/example/localloop/ui/auth/RegistrationActivity.java
+++ app/src/main/java/com/example/localloop/ui/auth/RegistrationActivity.java
@@ -15,11 +15,11 @@ import com.google.android.gms.tasks.Task;
 import androidx.annotation.NonNull;
 
 
+//Firebase guide https://firebase.google.com/docs/auth/android/password-auth#java_1
 public class RegistrationActivity extends AppCompatActivity {
     private static final String TAG = "RegistrationActivity";
 
     private EditText emailField, usernameField, passwordField;
-    private Button submitButton;
 
     private String email, username, password;
     private FirebaseAuth mAuth;
@@ -28,24 +28,28 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         mAuth = FirebaseAuth.getInstance();
+
         setContentView(R.layout.activity_registration);
 
         emailField = findViewById(R.id.inputEmail);
         usernameField = findViewById(R.id.inputUsername);
         passwordField = findViewById(R.id.inputPassword);
-        submitButton = findViewById(R.id.buttonSubmit);
+        Button submitButton = findViewById(R.id.buttonSubmit);
 
         submitButton.setOnClickListener(v -> {
             email = emailField.getText().toString().trim();
             username = usernameField.getText().toString().trim();
             password = passwordField.getText().toString().trim();
 
+            //basic check
             if(email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, "Email and password required", Toast.LENGTH_SHORT).show();
                 return;
             }
 
+            //firebase auth https://firebase.google.com/docs/auth/android/password-auth#java_1
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
@@ -67,6 +71,7 @@ public class RegistrationActivity extends AppCompatActivity {
         });
     }
 
+    //firebase auth https://firebase.google.com/docs/auth/android/password-auth#java_1
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
@@ -75,11 +80,34 @@ public class RegistrationActivity extends AppCompatActivity {
             reload();
         }
     }
+
+
     private void reload() {
-        // TODO: Implement reload logic
     }
 
+    //IMPORTANT TODO! Differenciate user type then send them into their corespond layout
     private void updateUI(FirebaseUser user) {
-        // TODO: Implement update UI logic
+        //SOMETHING LIKE below in loginActivity
+
+//        if (user != null) {
+//            String email = user.getEmail();
+//
+//            //Basic check
+//            if (email == null) {
+//                Toast.makeText(this, "User email not available", Toast.LENGTH_SHORT).show();
+//                return;
+//            }
+//
+//            //Hard coded
+//            if (1 == 1) {
+//                setContentView(R.layout.activity_organizer_home);
+//            } else if (email.contains("admin")) {
+//                setContentView(R.layout.activity_participant_home);
+//            } else {
+//                Toast.makeText(this, "Unrecognized user role", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+
+        
     }
 }
