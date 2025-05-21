0aff371287cddd81f6a2e57cf5489ca721388791
ShuChenDev <shu.chen.xm.work@gmail.com>
2025-05-20 22:24:19 -0400

    Formatted all files

--- app/src/main/java/com/example/localloop/ui/MainActivity.java
+++ app/src/main/java/com/example/localloop/ui/MainActivity.java
@@ -13,6 +13,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.example.localloop.ui.auth.Authenticator;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText usernameField;
@@ -51,11 +52,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = String.valueOf(usernameField.getText());
                 String password = String.valueOf(passwordField.getText());
-                if (auth.authenticateUser(username,password)) {
-                    Intent i = new Intent(MainActivity.this,Dashboard.class);
+                if (auth.authenticateUser(username, password)) {
+                    Intent i = new Intent(MainActivity.this, Dashboard.class);
                     startActivity(i);
-                }
-                else{
+                } else {
                     messageField.setText("wrong username/password");
                 }
 
